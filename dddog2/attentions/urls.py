c6d75326e3ffd4d830d1343830efe7e144d1684b
wangkaiquan <hope_courage@163.com>
2018-08-12 14:05:25 +0800

    add fan

--- dddog2/attentions/urls.py
+++ dddog2/attentions/urls.py
@@ -5,5 +5,7 @@ from attentions import views
 urlpatterns = [
     url(r'^test',views.test,name='test'),
     url(r'^attentions-cancels',views.attentions_cancels,name='attentions-cancels'),
+    url(r'^cancel-attentions',views.cancel_attentions,name='cancel-attentions'),
     url(r'^inquire-attentions',views.inquire_attentions,name='inquire-attentions'),
+    url(r'^inquire-fan',views.inquire_fan,name='inquire-fan'),
 ]
--- dddog2/attentions/views.py
+++ dddog2/attentions/views.py
@@ -48,7 +48,7 @@ def attentions_cancels(request):
                     result = {'respones': '已取消关注'}
                     return JsonResponse(result)
             else:
-                print(user_id,attention_id)
+                print(user_id, attention_id)
                 # 新建关注对象
                 new_attention = Attentions(user_id=user_id, attentions_id=attention_id)
                 try:
@@ -70,23 +70,50 @@ def attentions_cancels(request):
 
 def cancel_attentions(request):
     """取消关注"""
-    pass
+    # 获取用户id与关注id
+    user_id = request.session.get('id', '')
+    attention_id = request.POST.get('uid', '')
+    # 判断用户是否登陆
+    if user_id:
+        # 判断是否传入关注者
+        if attention_id:
+            # 获取关注对象
+            attention = Attentions.objects.get(user_id=user_id, attentions_id=attention_id)
+            # 将关注变更为取消关注
+            attention.is_cancel = 1
+            try:
+                attention.save()
+            except DatabaseError as e:
+                logging.warning(e)
+                result = {'respones': '取消关注异常'}
+                return JsonResponse(result)
+            result = {'respones': '已取消关注'}
+            return JsonResponse(result)
 
 
 def inquire_attentions(request):
     """查询关注"""
     # 获取用户id
     user_id = request.session.get('id', '')
-    # 获取用户对象
-    user_attentions = Attentions.objects.filter(user_id=user_id,is_cancel=0)
+    # 获取关注用户对象
+    user_attentions = Attentions.objects.filter(user_id=user_id, is_cancel=0)
     # 获取用户关注对象信息
     user_attentions_infos = []
     for user_attention in user_attentions:
-        user_attentions_infos.append([user_attention.attentions.id,user_attention.attentions.uname])
-    result = {'user_attentions_infos':user_attentions_infos}
+        user_attentions_infos.append([user_attention.attentions.id, user_attention.attentions.uname])
+    result = {'user_attentions_infos': user_attentions_infos}
     return JsonResponse(result)
 
 
 def inquire_fan(request):
     """查询粉丝"""
-    pass
+    # 获取用户id
+    user_id = request.session.get('id', '')
+    # 获取粉丝用户对象
+    user_fans = Attentions.objects.filter(attentions_id=user_id, is_cancel=0)
+    # 获取用户全部粉丝的信息
+    user_fans_infos = []
+    for user_fan in user_fans:
+        user_fans_infos.append([user_fan.user.id, user_fan.user.uname])
+    result = {'user_fans_infos': user_fans_infos}
+    return JsonResponse(result)
